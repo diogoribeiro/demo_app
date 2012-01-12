2f7a821b4e3d20d50c1173e313103e9600c8b459
Diogo Ribeiro <diogo.engsoftware@gmail.com>
2012-01-11 22:12:25 -0200

    Criando scaffold para micropost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120111150648) do
+ActiveRecord::Schema.define(:version => 20120112001058) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
